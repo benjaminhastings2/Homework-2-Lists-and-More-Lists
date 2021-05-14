1e77d5747f5bad48700ac13284820dec8e62d1aa
benjaminhastings2 <60204913+benjaminhastings2@users.noreply.github.com>
2021-05-13 23:04:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_Lists_and_More_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+
+            // Specify the data source.
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            // Define the query expression.
+            var badPasswords = from t in users
+                               where t.Password == "hello"
+                               select t.Name;
+
+            // Execute the query.
+            // part 1
+
+            Console.WriteLine("Bad Passwords:\n");
+            
+            foreach (var pw in badPasswords)
+            {
+                Console.WriteLine(pw);                
+            }
+
+            Console.WriteLine();
+
+            //part 2
+            users.RemoveAll(x => x.Password == x.Name.ToLower());
+
+            // part 3          
+            users.Remove((from a in users
+                               where a.Password == "hello"
+                               select a).First());
+
+            Console.WriteLine("User Directory:\n");
+
+            // part 4
+            foreach (var a in users)
+            {
+                Console.WriteLine($"Name: {a.Name} \t Password: {a.Password}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
